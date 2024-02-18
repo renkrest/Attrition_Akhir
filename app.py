3f7bbc30ac9d143e4260a020a3736e5e4f524205
renkrest <134942239+renkrest@users.noreply.github.com>
2024-02-18 12:37:17 +0700

    Update app.py

--- app.py
+++ app.py
@@ -30,9 +30,9 @@ if pilihan == 'Prediksi dari file csv':
         # Keputusan
         for i in range(len(hasil)):
             if hasil[i] == 1:
-                st.write('Data pelanggan',dataku['Loan ID'][i],'= diprediksi akan GAGAL BAYAR')
+                st.write('Attrition',dataku['Attrition'][i],'= diprediksi akan KELUAR PERUSAHAAN')
             else:
-                st.write('Data pelanggan',dataku['Loan ID'][i],'= diprediksi akan DAPAT MEMBAYAR')
+                st.write('Attrition',dataku['Attrition'][i],'= diprediksi akan KELUAR PERUSAHAAN')
     else:
         st.error('File yang diupload kosong, silakan pilih file yang valid')
         #st.markdown('File yang diupload kosong, silakan pilih file yang valid')
